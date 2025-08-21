7df032ab1c9feaa1aa5a50539ecff09707bef980
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2025-08-21 18:13:22 -0300

    Cria classe Residuo

---
+++ src/main/java/com/purpura/models/Residuo.java
@@ -0,0 +1,51 @@
+package com.purpura.models;
+
+public class Residuo {
+    // atributos
+    private final int nCdResiduo;
+    private String cNmResiduo;
+    private String cTipoUnidade;
+    private double nPrecoPadrao;
+    private double nVolumePadrao;
+    private String cCategoria;
+    private String cDescricao;
+
+    // construtor
+    public Residuo(int nCdResiduo, String cNmResiduo, String cTipoUnidade,
+                   double nPrecoPadrao, double nVolumePadrao, String cCategoria,
+                   String cDescricao){
+        this.nCdResiduo = nCdResiduo;
+        this.cNmResiduo = cNmResiduo;
+        this.cTipoUnidade = cTipoUnidade;
+        this.nPrecoPadrao = nPrecoPadrao;
+        this.nVolumePadrao = nVolumePadrao;
+        this.cCategoria = cCategoria;
+        this.cDescricao = cDescricao;
+    }
+
+    // getters
+    public int getNCdResiduo() {return nCdResiduo;}
+    public String getCNmResiduo() {return cNmResiduo;}
+    public String getCTipoUnidade() {return cTipoUnidade;}
+    public double getNPrecoPadrao() {return nPrecoPadrao;}
+    public double getNVolumePadrao() {return nVolumePadrao;}
+    public String getCCategoria() {return cCategoria;}
+    public String getCDescricao() {return cDescricao;}
+
+    // setters
+    public void setCNmResiduo(String cNmResiduo) {this.cNmResiduo = cNmResiduo;}
+    public void setCTipoUnidade(String cTipoUnidade) {this.cTipoUnidade = cTipoUnidade;}
+    public void setNPrecoPadrao(double nPrecoPadrao) {this.nPrecoPadrao = nPrecoPadrao;}
+    public void setNVolumePadrao(double nVolumePadrao) {this.nVolumePadrao = nVolumePadrao;}
+    public void setCCategoria(String cCategoria) {this.cCategoria = cCategoria;}
+    public void setCDescricao(String cDescricao) {this.cDescricao = cDescricao;}
+
+    // toString
+    @Override
+    public String toString(){
+        return "Código: " + nCdResiduo + "\nNome: " + cNmResiduo +
+                "\nTipo Unidade: " + cTipoUnidade + "\nPreço Padrão: " + nPrecoPadrao +
+                "\nVolume Padrão: " + nVolumePadrao + "\nCategoria: " + cCategoria +
+                "\nDescrição: " + cDescricao;
+    }
+}
