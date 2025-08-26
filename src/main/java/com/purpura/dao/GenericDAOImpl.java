915135df1cbb4926851f88c8f84eb3525b908ffd
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2025-08-26 19:04:10 -0300

    Cria método findAll e método abstrato mapResultSet

--- src/main/java/com/purpura/dao/GenericDAOImpl.java
+++ src/main/java/com/purpura/dao/GenericDAOImpl.java
@@ -4,7 +4,10 @@ import com.purpura.util.ConnectionFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class GenericDAOImpl<T> implements GenericDAO<T>{
     @Override
@@ -13,14 +16,31 @@ public abstract class GenericDAOImpl<T> implements GenericDAO<T>{
                 " WHERE nCd" + getTableName() + " = ?";
 
         try(Connection conn = ConnectionFactory.getConnection();
-            PreparedStatement stmt = conn.prepareStatement(sql)){{
+            PreparedStatement stmt = conn.prepareStatement(sql)){
                 stmt.setInt(1, id);
                 stmt.executeUpdate();
-            }
-        } catch (SQLException e){
+            } catch (SQLException e){
             e.printStackTrace();
         }
     }
 
+    public List<T> findAll() {
+        List<T> lista = new ArrayList<>();
+        String sql = "SELECT * FROM " + getTableName();
+
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+            while (rs.next()){
+                lista.add(mapResultSet(rs));
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return lista;
+    }
+
     public abstract String getTableName();
+    public abstract T mapResultSet(ResultSet rs) throws SQLException;
 }
