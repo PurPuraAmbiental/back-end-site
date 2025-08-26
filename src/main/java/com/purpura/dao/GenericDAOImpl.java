a232ee886532309473a318c7ac42fa5ba6680c30
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2025-08-26 18:27:11 -0300

    Cria classe abstrata que implementa GenericDAO, implementa método deleteById e cria método abstrato getTableName

---
+++ src/main/java/com/purpura/dao/GenericDAOImpl.java
@@ -0,0 +1,26 @@
+package com.purpura.dao;
+
+import com.purpura.util.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public abstract class GenericDAOImpl<T> implements GenericDAO<T>{
+    @Override
+    public void deleteById(int id){
+        String sql = "DELETE FROM " + getTableName() +
+                " WHERE nCd" + getTableName() + " = ?";
+
+        try(Connection conn = ConnectionFactory.getConnection()){
+            PreparedStatement stmt = conn.prepareStatement(sql){
+                stmt.setInt(1, id);
+                stmt.executeUpdate();
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public abstract String getTableName();
+}
