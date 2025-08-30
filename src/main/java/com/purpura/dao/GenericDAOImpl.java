9287800b75a83458f8cdf00a2cc0e83526e72891
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2025-08-30 15:14:11 -0300

    Cria métodos save, update e find no DAO abstrato

--- src/main/java/com/purpura/dao/GenericDAOImpl.java
+++ src/main/java/com/purpura/dao/GenericDAOImpl.java
@@ -11,9 +11,42 @@ import java.util.List;
 
 public abstract class GenericDAOImpl<T> implements GenericDAO<T>{
     @Override
+    public boolean save(T entidade) {
+        String sql = "INSERT INTO " + getNomeTabela() +
+                " (" + getNomesColunas() + ") VALUES (" + getPlaceholders() + ")";
+        try(Connection conn = ConnectionFactory.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            prepareStatementForSave(stmt, entidade);
+            int rows = stmt.executeUpdate();
+            return rows > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean update(T entidade) {
+        String sql = "UPDATE " + getNomeTabela() +
+                " SET " + getColunasUpdate() +
+                " WHERE " + getColunaId() + " = ?";
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            prepareStatementForUpdate(stmt, entidade);
+            int rows = stmt.executeUpdate();
+            return rows > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
     public boolean delete(int id){
-        String sql = "DELETE FROM " + getTableName() +
-                " WHERE nCd" + getTableName() + " = ?";
+        String sql = "DELETE FROM " + getNomeTabela() +
+                " WHERE " + getColunaId() + " = ?";
 
         try(Connection conn = ConnectionFactory.getConnection();
             PreparedStatement stmt = conn.prepareStatement(sql)){
@@ -27,9 +60,27 @@ public abstract class GenericDAOImpl<T> implements GenericDAO<T>{
     }
 
     @Override
+    public T find(int id) {
+        String sql = "SELECT * FROM " + getNomeTabela() +
+                " WHERE " + getColunaId() + " = ?";
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return mapResultSet(rs);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
     public List<T> findAll() {
         List<T> lista = new ArrayList<>();
-        String sql = "SELECT * FROM " + getTableName();
+        String sql = "SELECT * FROM " + getNomeTabela();
 
         try (Connection conn = ConnectionFactory.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql);
@@ -44,6 +95,12 @@ public abstract class GenericDAOImpl<T> implements GenericDAO<T>{
         return lista;
     }
 
-    public abstract String getTableName();
-    protected abstract T mapResultSet(ResultSet rs) throws SQLException;
+    public abstract String getNomeTabela(); // retorna nome da entidade
+    protected abstract T mapResultSet(ResultSet rs) throws SQLException; // retorna objeto da entidade
+    protected abstract String getNomesColunas(); // retorna o nome das colunas
+    protected abstract String getPlaceholders(); // retorna placeholders pra prepareStatement
+    protected abstract void prepareStatementForSave(PreparedStatement stmt, T entidade) throws SQLException; // prepareStatement pra método save
+    protected abstract void prepareStatementForUpdate(PreparedStatement stmt, T entidade) throws SQLException; // prepareStatement pra método update
+    protected abstract String getColunasUpdate(); // retorna as colunas para método update
+    protected abstract String getColunaId(); // retorna nome da coluna com id (primary key)
 }
