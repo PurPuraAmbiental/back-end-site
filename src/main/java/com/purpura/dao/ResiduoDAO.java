84cc4f63715641f2ef4d219b1ddfdc77306bc119
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2025-08-30 18:52:14 -0300

    ResiduoDAO deixa de implementar GenericDAO

--- src/main/java/com/purpura/dao/ResiduoDAO.java
+++ src/main/java/com/purpura/dao/ResiduoDAO.java
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class ResiduoDAO extends GenericDAOImpl<Residuo> implements GenericDAO<Residuo>{
+public class ResiduoDAO extends GenericDAOImpl<Residuo>{
     @Override
     public String getNomeTabela() {
         return "Residuo";
