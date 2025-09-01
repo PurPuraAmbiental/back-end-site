06edc24b8d1b3fa0d704508e868e5e9d261480da
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2025-09-01 12:19:13 -0300

    Adicionar variáveis de ambiente

--- src/main/java/com/purpura/util/ConnectionFactory.java
+++ src/main/java/com/purpura/util/ConnectionFactory.java
@@ -1,15 +1,18 @@
 package com.purpura.util;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
+    private static final Dotenv dotenv = Dotenv.load();
 
     // Lê as variáveis de ambiente do sistema
-    private static final String URL = System.getenv("DB_URL");
-    private static final String USER = System.getenv("DB_USER");
-    private static final String PASSWORD = System.getenv("DB_PASSWORD");
+    private static String URL = dotenv.get("DB_URL");
+    private static final String USER = dotenv.get("DB_USER");
+    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
 
     // Retorna uma conexão válida
     public static Connection getConnection() throws SQLException {
