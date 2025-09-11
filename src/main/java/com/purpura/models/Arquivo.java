dd80ab38c2ee4d12be33e2792c21161d3b9689a3
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2025-09-11 17:47:37 -0300

    (feat) Corrige atributos em Arquivo e cria construtor baseado em Map

--- src/main/java/com/purpura/models/Arquivo.java
+++ src/main/java/com/purpura/models/Arquivo.java
@@ -1,25 +1,39 @@
 package com.purpura.models;
-import com.purpura.models.enums.CategoriaArquivo;
+
+import java.text.ParseException;
 import java.time.LocalDate;
+import java.util.Map;
 
 public class Arquivo implements Model{
     // atributos
-    private final int nCdArquivo;
+    private int nCdArquivo;
     public String cNmArquivo;
     public String cTipoArquivo;
     public final LocalDate dArquivo;
-    public CategoriaArquivo categoriaArquivo;
-    public int nCdDono;
+    public final String cCnpj;
+    public final int nCdResiduo;
 
-    // construtor
+
+    // construtores
     public Arquivo(int nCdArquivo, String cNmArquivo, String cTipoArquivo,
-                   LocalDate dArquivo, CategoriaArquivo categoriaArquivo, int nCdEmpresa) {
+                   LocalDate dArquivo, String cCnpj, int nCdResiduo) {
         this.nCdArquivo = nCdArquivo;
         this.cNmArquivo = cNmArquivo;
         this.cTipoArquivo = cTipoArquivo;
         this.dArquivo = dArquivo;
-        this.categoriaArquivo = categoriaArquivo;
-        this.nCdDono = nCdEmpresa;
+        this.cCnpj = cCnpj;
+        this.nCdResiduo = nCdResiduo;
+    }
+
+    public Arquivo(Map<String, String> params) throws ParseException{
+        if(params.containsKey("nCdArquivo")){
+            this.nCdArquivo = Integer.parseInt(params.get("nCdArquivo"));
+        }
+        this.cNmArquivo = params.get("cNmArquivo");
+        this.cTipoArquivo = params.get("cTipoArquivo");
+        this.dArquivo = LocalDate.parse(params.get("dArquivo"));
+        this.cCnpj = params.get("cCnpj");
+        this.nCdResiduo = Integer.parseInt(params.get("nCdResiduo"));
     }
 
     // getters
@@ -27,21 +41,19 @@ public class Arquivo implements Model{
     public String getCNmArquivo(){return cNmArquivo;}
     public String getCTipoArquivo(){return cTipoArquivo;}
     public LocalDate getDArquivo(){return dArquivo;}
-    public CategoriaArquivo getCategoriaArquivo(){return categoriaArquivo;}
-    public int getNCdDono(){return nCdDono;}
+    public String getCCnpj(){return cCnpj;}
+    public int  getNCdResiduo(){return nCdResiduo;}
 
     // setters
     public void setCNmArquivo(String cNmArquivo){this.cNmArquivo = cNmArquivo;}
     public void setCTipoArquivo(String cTipoArquivo){this.cTipoArquivo = cTipoArquivo;}
-    public void setCategoriaArquivo(CategoriaArquivo categoriaArquivo){this.categoriaArquivo = categoriaArquivo;}
-    public void setNCdDono(int nCdDono){this.nCdDono = nCdDono;}
 
     // toString
     @Override
     public String toString(){
         return "Código: " + nCdArquivo + "\nNome: " + cNmArquivo +
                 "\nTipo: " + cTipoArquivo + "\nData: " + dArquivo +
-                "\nCategoria: " + categoriaArquivo + "\nDono: " + nCdDono;
+                "\nCNPJ: " + cCnpj + "\nResíduo: " + nCdResiduo;
     }
 
     // métodos
--- src/main/java/com/purpura/models/enums/CategoriaArquivo.java
+++
@@ -1,6 +0,0 @@
-package com.purpura.models.enums;
-
-public enum CategoriaArquivo {
-    EMPRESA,
-    RESIDUO
-}
