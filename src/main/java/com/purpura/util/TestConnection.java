fda185371309f9104e8c04b5e1356fadd41365ac
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2025-09-13 18:11:30 -0300

    (feat) Torna TestConnection boolean

--- src/main/java/com/purpura/util/TestConnection.java
+++ src/main/java/com/purpura/util/TestConnection.java
@@ -3,15 +3,18 @@ package com.purpura.util;
 import java.sql.*;
 
 public class TestConnection {
-    public static void main(String[] args) {
+    public static boolean main(String[] args) {
         try (Connection conn = ConnectionFactory.getConnection()) {
             if (conn != null && !conn.isClosed()) {
                 System.out.println("Conexão com o banco estabelecida com sucesso!");
+                return true;
             } else {
                 System.out.println(" Não foi possível estabelecer a conexão.");
             }
+            return false;
         } catch (SQLException e) {
             System.err.println("Erro ao conectar ao banco: " + e.getMessage());
+            return false;
         }
     }
 }
