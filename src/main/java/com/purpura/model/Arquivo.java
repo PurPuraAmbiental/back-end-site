a418bf0cd65c4a23103aa8c2a7aa1528d42aae1f
Bruna Oliveira14 <bruna.jesus@germinare.org.br>
2025-09-17 14:57:17 -0300

    (doc) - adicionando comentarios na classe Arquivo.java

--- src/main/java/com/purpura/model/Arquivo.java
+++ src/main/java/com/purpura/model/Arquivo.java
@@ -1,11 +1,13 @@
 package com.purpura.model;
-
 import java.text.ParseException;
 import java.time.LocalDate;
 import java.util.Map;
-
+/** Classe Modelo para o tabela Arquivo
+ *@author Kevin Oliveira
+ * */
 public class Arquivo implements Model{
-    // atributos
+    /**Adicionando os atributos da tabela como atributos da classe
+     * atributos dados com prefixos da tabela de banco de dado*/
     private int nCdArquivo;
     public String cNmArquivo;
     public String cTipoArquivo;
@@ -14,7 +16,14 @@ public class Arquivo implements Model{
     public final int nCdResiduo;
 
 
-    // construtores
+    /**Adicionando um metodo Construtor
+     * @param nCdArquivo - atributo identificador dessa instancia de Arquivo
+     * @param cNmArquivo - nome do arquivo
+     * @param cTipoArquivo - Tipo do conteudo do arquivo
+     * @param dUpload - Data do upload do arquivo
+     * @param cCnpj - Foring key da tabela Empresa
+     * @param nCdResiduo - Foring key da tabela Residuo
+     * */
     public Arquivo(int nCdArquivo, String cNmArquivo, String cTipoArquivo,
                    LocalDate dUpload, String cCnpj, int nCdResiduo) {
         this.nCdArquivo = nCdArquivo;
@@ -25,6 +34,8 @@ public class Arquivo implements Model{
         this.nCdResiduo = nCdResiduo;
     }
 
+    /**Construtor que iniciariza objetos
+     *@param params -> um map*/
     public Arquivo(Map<String, String> params) throws ParseException{
         if(params.containsKey("nCdArquivo")){
             this.nCdArquivo = Integer.parseInt(params.get("nCdArquivo"));
@@ -36,19 +47,34 @@ public class Arquivo implements Model{
         this.nCdResiduo = Integer.parseInt(params.get("nCdResiduo"));
     }
 
-    // getters
+    /**Metodo GetNCdArquivo
+     * @return Primary key */
     public int getNCdArquivo(){return nCdArquivo;}
+    /**Metodo GetCNmArquivo
+     * @return nome do arquivo*/
     public String getCNmArquivo(){return cNmArquivo;}
+    /**Metodo GetCTipoArquivo
+     * @return tipo de arquivo*/
     public String getCTipoArquivo(){return cTipoArquivo;}
+    /**Metodo GetDUpload
+     * @return - Data do upload*/
     public LocalDate getDUpload(){return dUpload;}
+    /**Metodo getCCnpj
+     * @return cnpj da Empresa*/
     public String getCCnpj(){return cCnpj;}
+    /**Metodo getNCdResiduo
+     *@return codigo da tabela residuo*/
     public int  getNCdResiduo(){return nCdResiduo;}
 
-    // setters
+    /**Metodo setCNmArquivo
+     * @param cNmArquivo*/
     public void setCNmArquivo(String cNmArquivo){this.cNmArquivo = cNmArquivo;}
+    /**Metodo setCTipoArquivo
+     * @param cTipoArquivo*/
     public void setCTipoArquivo(String cTipoArquivo){this.cTipoArquivo = cTipoArquivo;}
 
-    // toString
+    /**Metodo toString
+     *@return informação dos campos das tabelas*/
     @Override
     public String toString(){
         return "Código: " + nCdArquivo + "\nNome: " + cNmArquivo +
@@ -56,7 +82,8 @@ public class Arquivo implements Model{
                 "\nCNPJ: " + cCnpj + "\nResíduo: " + nCdResiduo;
     }
 
-    // métodos
+    /**Metodo para retornar a tabela
+     * @return nome da tabela*/
     @Override
     public Object getId(){return nCdArquivo;}
 }
