87da6e4a0f57039b66ad188fbb78c866189a24e6
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2025-09-19 09:17:12 -0300

    (bugfix) Corrige mensagem de erro em NotFoundException

--- src/main/java/com/purpura/exception/NotFoundException.java
+++ src/main/java/com/purpura/exception/NotFoundException.java
@@ -2,6 +2,6 @@ package com.purpura.exception;
 
 public class NotFoundException extends PurpuraException {
     public NotFoundException(String nomeTabela, Object id) {
-        super(String.format("Não encontrou na tabela %nome o id %s", nomeTabela, id));
+        super(String.format("Não encontrou na tabela %s o id %s", nomeTabela, id));
     }
 }
