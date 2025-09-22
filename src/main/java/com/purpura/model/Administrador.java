3a156a27c87ac0e218ea753ef2ca5f34bdfb0f3f
Bruna Oliveira14 <bruna.jesus@germinare.org.br>
2025-09-22 15:51:13 -0300

    (doc) - adicionando comentarios na classe Administrador.java

--- src/main/java/com/purpura/model/Administrador.java
+++ src/main/java/com/purpura/model/Administrador.java
@@ -3,19 +3,27 @@ package com.purpura.model;
 import java.text.ParseException;
 import java.util.Map;
 
+/**Classe Modelo para a tabela Administrador
+ * @author Kevin Oliveira*/
 public class Administrador implements Model{
-    // atributos
+    /**Adicionando os atributos da tabela como atributos da classe
+     * atributos dados com prefixos da tabela de banco de dados*/
     private String cEmail;
     private String cSenha;
     private String cNmAdministrador;
 
-    // construtores
+    /**Adicionando um método Construtor
+     * @param cEmail - email do administrador
+     * @param cSenha - senha do administrador
+     * @param cCnpj - nome do administrador (inserido incorretamente como cCnpj)*/
     public Administrador(String cEmail, String cSenha, String cCnpj) {
         this.cEmail = cEmail;
         this.cSenha = cSenha;
         this.cNmAdministrador = cCnpj;
     }
 
+    /**Adicionando um método construtor que inicializa objetos
+     * @param params -> um map com os parâmetros do administrador*/
     public Administrador(Map<String, String> params) throws ParseException {
         if(params.containsKey("cEmail")) {
             this.cEmail = params.get("cEmail");
@@ -24,25 +32,38 @@ public class Administrador implements Model{
         this.cNmAdministrador = params.get("cNmAdministrador");
     }
 
-    // getters
+    /**Método getCEmail
+     * @return email do administrador*/
     public String getCEmail() {return cEmail;}
+    /**Método getCSenha
+     * @return senha do administrador*/
     public String getCSenha() {return cSenha;}
+    /**Método getCNmAdministrador
+     * @return nome do administrador*/
     public String getCNmAdministrador() {return cNmAdministrador;}
 
-    // setters
+    /**Método setCEmail
+     * @param cEmail -> altera o email do administrador*/
     public void setCEmail(String cEmail) {this.cEmail = cEmail;}
+    /**Método setCSenha
+     * @param cSenha -> altera a senha do administrador*/
     public void setCSenha(String cSenha) {this.cSenha = cSenha;}
+    /**Método setCNmAdministrador
+     * @param cCnpj -> altera o nome do administrador (parametro nomeado como cCnpj)*/
     public void setCNmAdministrador(String cCnpj) {this.cNmAdministrador = cCnpj;}
 
-    // toString
+    /**Método toString
+     * @return informações sobre os campos da tabela Administrador*/
     @Override
     public String toString(){
         return "Email: " + cEmail + "\nSenha: " + cSenha + "\nNome: " + cNmAdministrador;
     }
 
-    // métodos
+    /**Método getId
+     * @return retorna o email como identificador da tabela*/
     @Override
     public Object getId(){
         return cEmail;
     }
 }
+
