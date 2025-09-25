ccf60a09a44023f3f5d78c1ce88a636d4ac2888a
EduardoRSilva5102 <eduardoultimate29@gmail.com.br>
2025-09-25 09:33:21 -0300

    (doc) - adicionando variavel de validacao de cnpj na classe Constants.java

--- src/main/java/com/purpura/common/Constants.java
+++ src/main/java/com/purpura/common/Constants.java
@@ -2,4 +2,5 @@ package com.purpura.common;
 
 public class Constants {
     public static final String COMMA_SEPARATOR_REGEX = "\\s*,\\s*";
+    public static final String EMAIL_REGEX = "^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
 }
