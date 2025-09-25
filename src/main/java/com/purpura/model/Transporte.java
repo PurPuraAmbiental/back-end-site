2ede5be56700d1ba082217aa348f32a407120f6d
Bruna Oliveira14 <bruna.jesus@germinare.org.br>
2025-09-25 09:12:08 -0300

    (doc) - adicionando comentarios na classe model Transporte.java

--- src/main/java/com/purpura/model/Transporte.java
+++ src/main/java/com/purpura/model/Transporte.java
@@ -3,14 +3,24 @@ package com.purpura.model;
 import java.time.LocalDate;
 import java.util.Map;
 
+/** Classe Modelo para a tabela Transporte
+ * Representa os transportes cadastrados no sistema,
+ * vinculados a pedidos e contendo informações sobre retirada.
+ * */
 public class Transporte implements Model{
-    // atributos
+    /**Adicionando os atributos da tabela como atributos da classe
+     * atributos dados com prefixos da tabela de banco de dado*/
     private int nCdTransporte;
     private String cNmTransporte;
     private LocalDate dRetirada;
     private final int nCdPedido;
 
-    // construtor
+    /**Adicionando um metodo Construtor
+     * @param nCdTransporte - atributo identificador dessa instancia de Transporte
+     * @param cNmTransporte - nome do transporte
+     * @param dRetirada - data de retirada do transporte
+     * @param nCdPedido - chave estrangeira da tabela Pedido
+     * */
     public Transporte(int nCdTransporte, String cNmTransporte, LocalDate dRetirada, int nCdPedido) {
         this.nCdTransporte = nCdTransporte;
         this.cNmTransporte = cNmTransporte;
@@ -18,6 +28,8 @@ public class Transporte implements Model{
         this.nCdPedido = nCdPedido;
     }
 
+    /**Construtor que inicializa objetos
+     *@param params -> um map*/
     public Transporte(Map<String, String> params) {
         if(params.containsKey("nCdTransporte")) {
             this.nCdTransporte = Integer.parseInt(params.get("nCdTransporte"));
@@ -27,24 +39,40 @@ public class Transporte implements Model{
         this.nCdPedido = Integer.parseInt(params.get("nCdPedido"));
     }
 
-    // getters
+    /**Metodo getNCdTransporte
+     * @return Primary key */
     public int getNCdTransporte(){return nCdTransporte;}
+
+    /**Metodo getCNmTransporte
+     * @return nome do transporte*/
     public String getCNmTransporte(){return cNmTransporte;}
+
+    /**Metodo getDRetirada
+     * @return data da retirada*/
     public LocalDate getDRetirada(){return dRetirada;}
+
+    /**Metodo getNCdPedido
+     * @return código do pedido vinculado*/
     public int getNCdPedido(){return nCdPedido;}
 
-    // setters
+    /**Metodo setCNmTransporte
+     * @param cNmTransporte*/
     public void setCNmTransporte(String cNmTransporte) {this.cNmTransporte = cNmTransporte;}
+
+    /**Metodo setDRetirada
+     * @param dRetirada*/
     public void setDRetirada(LocalDate dRetirada) {this.dRetirada = dRetirada;}
 
-    // toString
+    /**Metodo toString
+     *@return informação dos campos da tabela*/
     @Override
     public String toString(){
         return "Código: " + nCdTransporte + "\nNome: " + cNmTransporte +
                 "\nData Retirada: " + dRetirada + "\nPedido: " + nCdPedido;
     }
 
-    // métodos
+    /**Metodo para retornar a tabela
+     * @return nome da tabela*/
     @Override
     public Object getId(){return nCdTransporte;}
 }
