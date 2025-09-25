4cb3df78632fbc92c99431e9567e8eaf441e24b2
Bruna Oliveira14 <bruna.jesus@germinare.org.br>
2025-09-25 12:00:38 -0300

    (doc) - adicinando regez para email e telefone para email

--- src/main/java/com/purpura/common/Constants.java
+++ src/main/java/com/purpura/common/Constants.java
@@ -3,4 +3,6 @@ package com.purpura.common;
 public class Constants {
     public static final String COMMA_SEPARATOR_REGEX = "\\s*,\\s*";
     public static final String EMAIL_REGEX = "^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    public static final String TELEFONE_REGEX = "([\\d]{2})\\s?[\\d]{5}-[\\d]{4}";
 }
+
