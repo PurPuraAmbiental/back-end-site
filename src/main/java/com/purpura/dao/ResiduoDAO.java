0686eb9bd604ee4c6f4b9c1eaeda5d488ffdea44
Bruna Oliveira14 <bruna.jesus@germinare.org.br>
2025-10-01 22:12:08 -0300

    (doc) - adicionando comentarios na Classe DAO ResiduoDAO

--- src/main/java/com/purpura/dao/ResiduoDAO.java
+++ src/main/java/com/purpura/dao/ResiduoDAO.java
@@ -6,12 +6,23 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**Classe DAO para a tabela Residuo
+ * @author [Seu nome ou autor do código]*/
+
+/**Classe implementando a interface generica DAO
+ * e adicionando a Classe model equivalente: Residuo*/
 public class ResiduoDAO extends DAO<Residuo> {
+
+    /**Metodo para retornar o nome da tabela
+     * @return uma String com o nome da tabela*/
     @Override
     public String getNomeTabela() {
         return "Residuo";
     }
 
+    /**Metodo para instanciar um objeto
+     * @param rs -> ResultSet
+     * @return objeto Residuo*/
     @Override
     protected Residuo mapResultSet(ResultSet rs) throws SQLException {
         return new Residuo(
@@ -26,6 +37,8 @@ public class ResiduoDAO extends DAO<Residuo> {
         );
     }
 
+    /**Adicionando Metodo para pegar o nome das colunas
+     * @return String com os nomes dos atributos da model*/
     @Override
     protected String getNomesColunas() {
         return "cNmResiduo, cTipoUnidade, " +
@@ -33,6 +46,9 @@ public class ResiduoDAO extends DAO<Residuo> {
                 "cDescricao, cCnpj";
     }
 
+    /**Adicionando metodo para Inserir conteudo no banco de dados
+     * @param stmt -> String com o comando sql
+     * @param entidade -> nome da tabela*/
     @Override
     protected void prepareStatementForSave(PreparedStatement stmt, Residuo entidade) throws SQLException {
         stmt.setString(1, entidade.getCNmResiduo());
@@ -44,12 +60,17 @@ public class ResiduoDAO extends DAO<Residuo> {
         stmt.setString(7, entidade.getCCnpj());
     }
 
+    /**Adicionando metodo para Atualizar conteudo no banco de dados
+     * @param stmt -> String com o comando sql
+     * @param entidade -> nome da tabela*/
     @Override
     protected void prepareStatementForUpdate(PreparedStatement stmt, Residuo entidade) throws SQLException {
         prepareStatementForSave(stmt, entidade);
         stmt.setInt(8, entidade.getNCdResiduo());
     }
 
+    /**Adcionando Metodo para buscar a primary key da coluna
+     * @return chave primaria da coluna*/
     @Override
     protected String getColunaId() {
         return "nCdResiduo";
