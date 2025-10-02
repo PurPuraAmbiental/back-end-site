9ce7a3332874dfb9679fca51f90f665ceab97e1e
Bruna Oliveira14 <bruna.jesus@germinare.org.br>
2025-10-01 22:12:18 -0300

    (doc) - adicionando comentarios na Classe DAO TelefoneDAO

--- src/main/java/com/purpura/dao/TelefoneDAO.java
+++ src/main/java/com/purpura/dao/TelefoneDAO.java
@@ -6,12 +6,25 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**Classe DAO para a tabela Telefone
+ * @author [Seu nome ou autor do código]*/
+
+/**Classe implementando a interface generica DAO
+ * e adicionando a Classe model equivalente: Telefone*/
 public class TelefoneDAO extends DAO<Telefone> {
+
+    /**Metodo para retornar o nome da tabela
+     * @return uma String com o nome da tabela*/
     @Override
     public String getNomeTabela() {
         return "Telefone";
     }
-    protected Telefone mapResultSet(ResultSet rs) throws SQLException{
+
+    /**Metodo para instanciar um objeto
+     * @param rs -> ResultSet
+     * @return objeto Telefone*/
+    @Override
+    protected Telefone mapResultSet(ResultSet rs) throws SQLException {
         return new Telefone(
                 rs.getInt("nCdTelefone"),
                 rs.getInt("nCdEmpresa"),
@@ -20,11 +33,16 @@ public class TelefoneDAO extends DAO<Telefone> {
         );
     }
 
+    /**Adicionando Metodo para pegar o nome das colunas
+     * @return String com os nomes dos atributos da model*/
     @Override
     protected String getNomesColunas() {
         return "nCdTelefone, fone, nCdEmpresa, Descricao";
     }
 
+    /**Adicionando metodo para Inserir conteudo no banco de dados
+     * @param stmt -> String com o comando sql
+     * @param entidade -> nome da tabela*/
     @Override
     protected void prepareStatementForSave(PreparedStatement stmt, Telefone entidade) throws SQLException {
         stmt.setInt(1, entidade.getnCdEmpresa());
@@ -33,12 +51,17 @@ public class TelefoneDAO extends DAO<Telefone> {
         stmt.setString(4, entidade.getCDescricao());
     }
 
+    /**Adicionando metodo para Atualizar conteudo no banco de dados
+     * @param stmt -> String com o comando sql
+     * @param entidade -> nome da tabela*/
     @Override
     protected void prepareStatementForUpdate(PreparedStatement stmt, Telefone entidade) throws SQLException {
         prepareStatementForSave(stmt, entidade);
         stmt.setInt(9, entidade.getnCdTelefone());
     }
 
+    /**Adcionando Metodo para buscar a primary key da coluna
+     * @return chave primaria da coluna*/
     @Override
     protected String getColunaId() {
         return "nCdTelefone";
