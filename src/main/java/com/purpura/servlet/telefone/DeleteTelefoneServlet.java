f4cb29cc756be0a455deaec943e67d40de70a186
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2025-10-13 21:21:58 -0300

    (feat) Cria servlets para Telefone

---
+++ src/main/java/com/purpura/servlet/telefone/DeleteTelefoneServlet.java
@@ -0,0 +1,31 @@
+package com.purpura.servlet.telefone;
+
+import com.purpura.dao.DAO;
+import com.purpura.dao.TelefoneDAO;
+import com.purpura.exception.ConnectionFailedException;
+import com.purpura.exception.NotFoundException;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "DeleteTelefoneServlet", value = "/telefone/delete")
+public class DeleteTelefoneServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws jakarta.servlet.ServletException, IOException {
+        String idStr = request.getParameter("nCdTelefone");
+        try {
+            int id = Integer.parseInt(idStr);
+            DAO<?> dao = new TelefoneDAO();
+            dao.delete(id);
+            response.sendRedirect(request.getContextPath() + "/telefone/list");
+        } catch (ConnectionFailedException | NotFoundException | NumberFormatException e) {
+            request.setAttribute("erro", "Erro ao deletar Telefone: " + e.getMessage());
+            RequestDispatcher rd = request.getRequestDispatcher("/erro.jsp");
+            rd.forward(request, response);
+        }
+    }
+}
---
+++ src/main/java/com/purpura/servlet/telefone/UpdateTelefoneServlet.java
@@ -0,0 +1,36 @@
+package com.purpura.servlet.telefone;
+
+import com.purpura.dao.DAO;
+import com.purpura.dao.TelefoneDAO;
+import com.purpura.exception.ConnectionFailedException;
+import com.purpura.exception.NotFoundException;
+import com.purpura.model.Telefone;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@WebServlet(name = "UpdateTelefoneServlet", value = "/telefone/update")
+public class UpdateTelefoneServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws jakarta.servlet.ServletException, IOException {
+        try {
+            Map<String, String> params = new LinkedHashMap<>();
+            request.getParameterMap().forEach((key, values) -> params.put(key, values[0]));
+            Telefone model = new Telefone(params);
+            DAO<Telefone> dao = new TelefoneDAO();
+            dao.update(model);
+            response.sendRedirect(request.getContextPath() + "/telefone/list");
+        } catch (ConnectionFailedException | NotFoundException | ParseException | NumberFormatException e) {
+            request.setAttribute("erro", "Erro ao atualizar Telefone: " + e.getMessage());
+            RequestDispatcher rd = request.getRequestDispatcher("/erro.jsp");
+            rd.forward(request, response);
+        }
+    }
+}
