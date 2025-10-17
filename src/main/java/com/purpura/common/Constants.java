33bd8e4e1b239cbd65db9bb4017ee93c5fabe1ee
BrunaOliveira14 <bruna.jesus@germinare.org.br>
2025-10-17 20:51:12 -0300

    (bugfix) - concertando erro de verificação na jsp e na expressao regular

--- src/main/java/com/purpura/common/Constants.java
+++ src/main/java/com/purpura/common/Constants.java
@@ -2,8 +2,9 @@ package com.purpura.common;
 
 public class Constants {
     public static final String COMMA_SEPARATOR_REGEX = "\\s*,\\s*";
-    public static final String EMAIL_REGEX = "^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\.com(\\.[a-zA-Z])*";
+    public static final String EMAIL_REGEX = "^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+";
     public static final String TELEFONE_REGEX = "([\\d]{2})\\s?[\\d]{5}-[\\d]{4}";
     public static final String CNPJ_REGEX = "[0-9]{2}\\.?[0-9]{3}\\.?[0-9]{3]/?[0-9]{4}-?[0-9]{2}";
+    public static final String SENHA_REGEX = "[\\w]{6,}";
 }
 
