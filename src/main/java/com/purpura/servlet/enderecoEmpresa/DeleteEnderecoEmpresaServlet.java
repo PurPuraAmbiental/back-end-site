6881c88f67d8aaf75124f058727d1742de9bcf47
BrunaOliveira14 <bruna.jesus@germinare.org.br>
2025-10-21 19:53:09 -0300

    (bugfix) - Concertando o delete de endereco

--- src/main/java/com/purpura/servlet/enderecoEmpresa/DeleteEnderecoEmpresaServlet.java
+++ src/main/java/com/purpura/servlet/enderecoEmpresa/DeleteEnderecoEmpresaServlet.java
@@ -1,9 +1,12 @@
 package com.purpura.servlet.enderecoEmpresa;
 
 import com.purpura.dao.DAO;
+import com.purpura.dao.EmpresaDAO;
 import com.purpura.dao.EnderecoEmpresaDAO;
+import com.purpura.dto.EnderecoEmpresaView;
 import com.purpura.exception.ConnectionFailedException;
 import com.purpura.exception.NotFoundException;
+import com.purpura.model.Empresa;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,17 +14,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "DeleteEnderecoEmpresaServlet", value = "/endereco-empresa/delete")
 public class DeleteEnderecoEmpresaServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws jakarta.servlet.ServletException, IOException {
-        String idStr = request.getParameter("nCdEnderecoEmpresa");
+        String idEndereco = request.getParameter("nCdEnderecoEmpresa");
         try {
-            int id = Integer.parseInt(idStr);
-            DAO<?> dao = new EnderecoEmpresaDAO();
+            int id = Integer.parseInt(idEndereco);
+            EnderecoEmpresaDAO dao = new EnderecoEmpresaDAO();
             dao.delete(id);
+            List<EnderecoEmpresaView> endereco = dao.listarComEmpresa();
+            request.setAttribute("listaEnderecos", endereco);
             request.getRequestDispatcher("/CRUD/endereco.jsp").forward(request, response);
         } catch (ConnectionFailedException | NotFoundException | NumberFormatException e) {
             request.setAttribute("erro", "Erro ao deletar EnderecoEmpresa: " + e.getMessage());
