6f94672f5caebcf56dccedb4747f82d3c103258b
BrunaOliveira14 <bruna.jesus@germinare.org.br>
2025-10-25 14:19:24 -0300

    (feat) - adicionando validação de dados no insert da empresa

--- src/main/java/com/purpura/servlet/empresa/InsertEmpresaServlet.java
+++ src/main/java/com/purpura/servlet/empresa/InsertEmpresaServlet.java
@@ -12,7 +12,6 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.LinkedHashMap;
@@ -23,33 +22,53 @@ import java.util.Map;
 public class InsertEmpresaServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws jakarta.servlet.ServletException, IOException {
-        String email = request.getParameter("cEmail");
-        String cnpj = request.getParameter("cCnpj");
-        if (!Regex.validarEmail(email)) {
-            request.setAttribute("erro", "Email inválido!");
-            request.getRequestDispatcher("/CRUD/empresas.jsp").forward(request, response);
-            return;  // Impede que o código continue
-        }
 
-        // Validação de CNPJ
-        if (!Regex.validarSenha(cnpj)) {
-            request.setAttribute("erro", "CNPJ inválido!");
-            request.getRequestDispatcher("/CRUD/empresas.jsp").forward(request, response); // Mantém o popup aberto
-            return;  // Impede que o código continue
-        }
-
-        try {
+            try {
             Map<String, String> params = new LinkedHashMap<>();
             request.getParameterMap().forEach((key, values) -> params.put(key, values[0]));
-            if (params.containsKey("cSenha")) {
-                String hash = Criptografia.criptografar(params.get("cSenha"));
-                params.put("cSenha", hash);
-            }
+
             Empresa model = new Empresa(params);
             DAO<Empresa> dao = new EmpresaDAO();
+            List<?> empresas = dao.findAll();
+                //VALIDAÇAO DE DADOS
+            request.setAttribute("listaEmpresas",  empresas);
+            if (!Regex.validarEmail(model.getCEmail())) {
+                request.setAttribute("erro", "Não foi possivel cadastrar Empresa! \n Digite um e-mail valido");
+                request.getRequestDispatcher("/CRUD/empresas.jsp").forward(request, response);
+                return;  // Impede que o código continue
+            }
+            if (!Regex.validarCnpj(model.getCCnpj())) {
+                request.setAttribute("erro", "Não foi possivel cadastrar Empresa! \n Digite um cnpj valido");
+                request.getRequestDispatcher("/CRUD/empresas.jsp").forward(request, response); // Mantém o popup aberto
+                return;  // Impede que o código continue
+            } else {
+                boolean continuar = true;
+                for (int i =0; i < model.getCCnpj().length() && continuar; i++){
+                    if (model.getCCnpj().charAt(i) == '/' || model.getCCnpj().charAt(i) == '.' || model.getCCnpj().charAt(i) == '-') {
+                        continuar = false;
+                        model.setcCnpj(model.getCCnpj().replace("/", ""));
+                        model.setcCnpj(model.getCCnpj().replace(".", ""));
+                        model.setcCnpj(model.getCCnpj().replace("-", ""));
+                        System.out.println(model.getCCnpj());
+                    }
+                }
+            }
+            if (model.getCSenha().length() < 6){
+                request.setAttribute("erro", "Não foi possivel cadastrar Empresa! \n Sua senha deve ter 6 ou mais caracteres validos");
+                request.getRequestDispatcher("/CRUD/empresas.jsp").forward(request, response);
+                System.out.println(model.getCSenha());
+                return;
+            }
+
+            if (params.containsKey("cSenha")) {
+                    String hash = Criptografia.criptografar(params.get("cSenha"));
+                    params.put("cSenha", hash);
+                }
+
+
             dao.save(model);
 
-            List<?> empresas = dao.findAll();
+            empresas = dao.findAll();
             request.setAttribute("listaEmpresas",  empresas);
             request.getRequestDispatcher("/CRUD/empresas.jsp").forward(request, response);
         } catch (ConnectionFailedException | NotFoundException e) {
