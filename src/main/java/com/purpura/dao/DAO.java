bf58df68a84c623717a6e16b33423177e890026d
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2025-10-25 20:11:55 -0300

    (refactor) Comenta toda a classe DAO

--- src/main/java/com/purpura/dao/DAO.java
+++ src/main/java/com/purpura/dao/DAO.java
@@ -14,7 +14,24 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Classe abstrata DAO genérica responsável pelas operações básicas envolvendo banco de dados
+ * de entidades que implementam a interface Model.
+ *
+ * @param <T> Tipo da entidade que estende Model
+ *
+ * @author Kevin de Oliveira
+ * @author Bruna de Jesus
+ */
 public abstract class DAO<T extends Model> {
+
+    /**
+     * Salva a entidade no banco de dados.
+     *
+     * @param entidade Objeto da entidade a ser salvo
+     * @throws NotFoundException Se nenhuma linha foi inserida
+     * @throws ConnectionFailedException Se houver falha na conexão ou execução SQL
+     */
     public void save(T entidade) throws NotFoundException, ConnectionFailedException {
         String sql = "INSERT INTO " + getNomeTabela() +
                 " (" + getNomesColunas() + ") VALUES (" + getPlaceholders() + ")";
@@ -33,6 +50,13 @@ public abstract class DAO<T extends Model> {
         }
     }
 
+    /**
+     * Atualiza a entidade no banco de dados.
+     *
+     * @param entidade Objeto da entidade a ser atualizado
+     * @throws NotFoundException Se nenhuma linha foi atualizada
+     * @throws ConnectionFailedException Se houver falha na conexão ou execução SQL
+     */
     public void update(T entidade) throws NotFoundException, ConnectionFailedException{
         String sql = "UPDATE " + getNomeTabela() +
                 " SET " + getColunasUpdate() +
@@ -49,25 +73,13 @@ public abstract class DAO<T extends Model> {
         }
     }
 
-    public void updateByAttribute(T entidade, String atributo, Object valor) throws NotFoundException, ConnectionFailedException{
-        String sql = "UPDATE " + getNomeTabela() +
-                " SET " + getNomesColunas() +
-                " WHERE " + atributo + " = ?";
-        try (Connection conn = ConnectionFactory.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
-            prepareStatementForUpdate(stmt, entidade);
-
-            int numeroColunas = getNomesColunas().split(Constants.COMMA_SEPARATOR_REGEX).length;
-            stmt.setObject(numeroColunas + 1, valor);
-            int rows = stmt.executeUpdate();
-            if(rows == 0) {
-                throw new NotFoundException(getNomeTabela(), entidade.getId());
-            }
-        } catch (SQLException e) {
-            throw new ConnectionFailedException();
-        }
-    }
-
+    /**
+     * Deleta um item do banco de dados a partir de id numérico
+     *
+     * @param id Identificador numérico do item a ser apagado
+     * @throws NotFoundException Se nenhuma linha foi deletada
+     * @throws ConnectionFailedException Se houver falha na conexão ou execução SQL
+     */
     public void delete(int id) throws NotFoundException, ConnectionFailedException{
         String sql = "DELETE FROM " + getNomeTabela() +
                 " WHERE " + getColunaId() + " = ?";
@@ -84,6 +96,13 @@ public abstract class DAO<T extends Model> {
         }
     }
 
+    /**
+     * Deleta um item do banco de dados a partir de id textual
+     *
+     * @param id Identificador textual do item a ser apagado
+     * @throws NotFoundException Se nenhuma linha foi deletada
+     * @throws ConnectionFailedException Se houver falha na conexão ou execução SQL
+     */
     public void delete(String id) throws NotFoundException, ConnectionFailedException{
         String sql = "DELETE FROM " + getNomeTabela() +
                 " WHERE " + getColunaId() + " = ?";
@@ -100,6 +119,13 @@ public abstract class DAO<T extends Model> {
         }
     }
 
+    /**
+     * Deleta registros com base em um atributo específico.
+     *
+     * @param atributo Nome da coluna usada como referência
+     * @param valor Valor da coluna de referência
+     * @throws ConnectionFailedException Se houver falha na conexão ou execução SQL
+     */
     public void deleteByAttribute(String atributo, Object valor) throws NotFoundException, ConnectionFailedException{
         String sql = "DELETE FROM " + getNomeTabela() +
                 " WHERE " + atributo + " = ?";
@@ -113,6 +139,14 @@ public abstract class DAO<T extends Model> {
         }
     }
 
+    /**
+     * Busca uma entidade pelo id numérico.
+     *
+     * @param id Chave primária da entidade
+     * @return Entidade encontrada
+     * @throws NotFoundException Se nenhuma entidade for encontrada
+     * @throws ConnectionFailedException Se houver falha na conexão ou execução SQL
+     */
     public T findById(int id) throws NotFoundException, ConnectionFailedException {
         String sql = "SELECT * FROM " + getNomeTabela() +
                 " WHERE " + getColunaId() + " = ?";
@@ -131,6 +165,14 @@ public abstract class DAO<T extends Model> {
         }
     }
 
+    /**
+     * Busca uma entidade pelo id textual.
+     *
+     * @param id Chave primária da entidade
+     * @return Entidade encontrada
+     * @throws NotFoundException Se nenhuma entidade for encontrada
+     * @throws ConnectionFailedException Se houver falha na conexão ou execução SQL
+     */
     public T findById(String id) throws NotFoundException, ConnectionFailedException {
         String sql = "SELECT * FROM " + getNomeTabela() +
                 " WHERE " + getColunaId() + " = ?";
@@ -149,6 +191,15 @@ public abstract class DAO<T extends Model> {
         }
     }
 
+    /**
+     * Busca a primeira entidade que corresponda a um atributo específico.
+     *
+     * @param coluna Nome da coluna usada como filtro
+     * @param valor Valor da coluna
+     * @return Entidade encontrada
+     * @throws NotFoundException Se nenhuma entidade for encontrada
+     * @throws ConnectionFailedException Se houver falha na conexão ou execução SQL
+     */
     public T findByAttribute(String coluna, Object valor) throws NotFoundException, ConnectionFailedException {
         String sql = "SELECT * FROM " + getNomeTabela() +
                 " WHERE " + coluna + " = ?";
@@ -168,6 +219,14 @@ public abstract class DAO<T extends Model> {
         }
     }
 
+    /**
+     * Busca todas as entidades que correspondam a um atributo específico.
+     *
+     * @param coluna Nome da coluna usada como filtro
+     * @param valor Valor da coluna
+     * @return Lista de entidades encontradas
+     * @throws ConnectionFailedException Se houver falha na conexão ou execução SQL
+     */
     public List<T> findAllByAttribute(String coluna, Object valor)
             throws ConnectionFailedException {
         String sql = "SELECT * FROM " + getNomeTabela() + " WHERE " + coluna + " = ?";
@@ -191,7 +250,13 @@ public abstract class DAO<T extends Model> {
         }
     }
 
-
+    /**
+     * Busca todas as entidades da tabela.
+     *
+     * @return Lista de entidades
+     * @throws NotFoundException Se não houver registros
+     * @throws ConnectionFailedException Se houver falha na conexão ou execução SQL
+     */
     public List<T> findAll() throws NotFoundException, ConnectionFailedException{
         List<T> lista = new ArrayList<>();
         String sql = "SELECT * FROM " + getNomeTabela();
@@ -214,41 +279,23 @@ public abstract class DAO<T extends Model> {
         return lista;
     }
 
-    public ResultSet find() throws NotFoundException, ConnectionFailedException {
-        String sql = "SELECT * FROM " + getNomeTabela();
-        ResultSet rs = null;
+    // ----- MÉTODOS UTILITÁRIOS -----
 
-        try (Connection conn = ConnectionFactory.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
-            rs = stmt.executeQuery();  // Executa a consulta e obtém o ResultSet
-        } catch (SQLException e) {
-            throw new ConnectionFailedException();
-        }
-
-        // Verifica se o ResultSet está vazio
-        try {
-            if (rs != null && !rs.next()) {
-                throw new NotFoundException(getNomeTabela(), -1);
-            }
-            // Volta o ponteiro para o início do ResultSet
-            if (rs != null) {
-                rs.beforeFirst(); // Vai para o começo, pois rs.next() já avançaria para a primeira linha
-            }
-        } catch (SQLException e) {
-            throw new ConnectionFailedException();
-        }
-
-        return rs;  // Retorna o ResultSet
-    }
-
-
-
-
-    // métodos utilitários
+    /**
+     * Gera um String composta de um "?" para cada coluna da tabela
+     *
+     * @return String contendo um "?" para cada coluna da tabela.
+     */
     protected String getPlaceholders() {
         int quantidade = getNomesColunas().split(Constants.COMMA_SEPARATOR_REGEX).length;
         return String.join(", ", Collections.nCopies(quantidade, "?"));
     }
+
+    /**
+     * Gera a string usada na cláusula SET do UPDATE, ignorando a coluna de ID.
+     *
+     * @return String no formato "coluna1 = ?, coluna2 = ?, ...", pronta para o PreparedStatement.
+     */
     protected String getColunasUpdate() {
         String[] colunas = getNomesColunas().split(Constants.COMMA_SEPARATOR_REGEX);
         String colunaId = getColunaId();
@@ -262,10 +309,51 @@ public abstract class DAO<T extends Model> {
         return String.join(", ", updates);
     }
 
-    public abstract String getNomeTabela(); // retorna nome da entidade
-    protected abstract T mapResultSet(ResultSet rs) throws SQLException; // retorna objeto da entidade
-    protected abstract String getNomesColunas(); // retorna o nome das colunas
+    /**
+     * Retorna o nome da tabela associada ao DAO.
+     *
+     * @return Nome da tabela no banco de dados.
+     */
+    public abstract String getNomeTabela();
+
+    /**
+     * Mapeia um ResultSet para um objeto da entidade correspondente.
+     *
+     * @param rs ResultSet retornado pela query SQL
+     * @return Objeto da entidade T preenchido com os dados do ResultSet
+     * @throws SQLException Se ocorrer algum erro ao acessar os dados do ResultSet
+     */
+    protected abstract T mapResultSet(ResultSet rs) throws SQLException;
+
+    /**
+     * Retorna os nomes das colunas da tabela, separados por vírgula.
+     *
+     * @return String contendo os nomes das colunas
+     */
+    protected abstract String getNomesColunas();
+
+    /**
+     * Prepara um PreparedStatement para salvar a entidade no banco.
+     *
+     * @param stmt PreparedStatement a ser preparado
+     * @param entidade Entidade que será salva
+     * @throws SQLException Se ocorrer algum erro ao setar os parâmetros do PreparedStatement
+     */
     protected abstract void prepareStatementForSave(PreparedStatement stmt, T entidade) throws SQLException; // prepareStatement pra método save
+
+    /**
+     * Prepara um PreparedStatement para atualizar a entidade no banco.
+     *
+     * @param stmt PreparedStatement a ser preparado
+     * @param entidade Entidade que será atualizada
+     * @throws SQLException Se ocorrer algum erro ao setar os parâmetros do PreparedStatement
+     */
     protected abstract void prepareStatementForUpdate(PreparedStatement stmt, T entidade) throws SQLException; // prepareStatement pra método update
+
+    /**
+     * Retorna o nome da coluna que funciona como ID (primary key) da tabela.
+     *
+     * @return Nome da coluna ID
+     */
     protected abstract String getColunaId(); // retorna nome da coluna com id (primary key)
 }
