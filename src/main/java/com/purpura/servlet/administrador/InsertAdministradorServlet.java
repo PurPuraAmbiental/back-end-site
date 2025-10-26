f4da9ce1f7378509cc7306d27b24e7481bb80338
BrunaOliveira14 <bruna.jesus@germinare.org.br>
2025-10-26 11:13:22 -0300

    (feat) - adicionando validação de dados nos inserts do Administrador

--- src/main/java/com/purpura/servlet/administrador/InsertAdministradorServlet.java
+++ src/main/java/com/purpura/servlet/administrador/InsertAdministradorServlet.java
@@ -1,10 +1,12 @@
 package com.purpura.servlet.administrador;
 
+import com.purpura.common.Regex;
 import com.purpura.dao.AdministradorDAO;
 import com.purpura.dao.DAO;
 import com.purpura.exception.ConnectionFailedException;
 import com.purpura.exception.NotFoundException;
 import com.purpura.model.Administrador;
+import com.purpura.model.Empresa;
 import com.purpura.util.Criptografia;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.annotation.WebServlet;
@@ -15,6 +17,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 @WebServlet(name = "InsertAdministradorServlet", value = "/administrador/insert")
@@ -24,14 +27,35 @@ public class InsertAdministradorServlet extends HttpServlet {
         try {
             Map<String, String> params = new LinkedHashMap<>();
             request.getParameterMap().forEach((key, values) -> params.put(key, values[0]));
-            if (params.containsKey("cSenha")) {
-                String hash = Criptografia.criptografar(params.get("cSenha"));
-                params.put("cSenha", hash);
-            }
             Administrador model = new Administrador(params);
             DAO<Administrador> dao = new AdministradorDAO();
+            List<?> administrador = dao.findAll();
+            Administrador existente = dao.findById(model.getCEmail());
+            if (!Regex.validarEmail(model.getCEmail())){
+                request.setAttribute("erro", "Não foi possivel cadastrar Administrador. Insira um E-mail valido");
+                request.setAttribute("listaAdministradores", administrador);
+                request.getRequestDispatcher("/CRUD/administrador.jsp").forward(request, response);
+                return;
+            } else if (existente != null) {
+                request.setAttribute("erro", "Não foi possivel cadastrar Administrador. E-mail ja cadastrado anteriormente");
+                request.setAttribute("listaAdministradores", administrador);
+                request.getRequestDispatcher("/CRUD/administrador.jsp").forward(request, response);
+                return;
+            } else if (model.getCSenha().length() < 6) {
+                request.setAttribute("erro", "Não foi possivel cadastrar Administrador. A senha deve possuir no minimo 6 caracteres");
+                request.setAttribute("listaAdministradores", administrador);
+                request.getRequestDispatcher("/CRUD/administrador.jsp").forward(request, response);
+                return;
+            } else {
+                if (params.containsKey("cSenha")) {
+                    String hash = Criptografia.criptografar(params.get("cSenha"));
+                    params.put("cSenha", hash);
+                }
+            }
+            administrador = dao.findAll();
             dao.save(model);
-            response.sendRedirect(request.getContextPath() + "/administrador/list");
+            request.setAttribute("listaAdministradores", administrador);
+            request.getRequestDispatcher("/CRUD/administrador.jsp").forward(request, response);
         } catch (ConnectionFailedException | NotFoundException e) {
             request.setAttribute("erro", "Erro ao inserir Administrador: " + e.getMessage());
             RequestDispatcher rd = request.getRequestDispatcher("/erro.jsp");
