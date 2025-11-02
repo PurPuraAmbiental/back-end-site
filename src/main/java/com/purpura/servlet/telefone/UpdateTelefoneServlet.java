2e1baa187f564e0e61e5a22c83a917fad66761d2
BrunaOliveira14 <bruna.jesus@germinare.org.br>
2025-11-02 14:10:20 -0300

    (feat) - adicionando mensagem de erro para evitar o cadastro e update de telefone em empresas inativas

--- src/main/java/com/purpura/servlet/telefone/UpdateTelefoneServlet.java
+++ src/main/java/com/purpura/servlet/telefone/UpdateTelefoneServlet.java
@@ -1,10 +1,9 @@
 package com.purpura.servlet.telefone;
 
-import com.purpura.common.ErroServlet;
 import com.purpura.common.Regex;
-import com.purpura.dao.DAO;
 import com.purpura.dao.EmpresaDAO;
 import com.purpura.dao.TelefoneDAO;
+import com.purpura.dto.TelefoneView;
 import com.purpura.exception.ConnectionFailedException;
 import com.purpura.exception.NotFoundException;
 import com.purpura.model.Empresa;
@@ -16,17 +15,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.text.ParseException;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 @WebServlet(name = "UpdateTelefoneServlet", value = "/telefone/update")
 public class UpdateTelefoneServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws jakarta.servlet.ServletException, IOException {
-        DAO<Telefone> dao = new TelefoneDAO();
+        TelefoneDAO telefoneDAO = new TelefoneDAO();
         String lista = "listaTelefones";
-        String caminho = "WEB-INF/CRUD/telefone.jsp";
+        String caminho = "/WEB-INF/CRUD/telefone.jsp";
         try {
             Map<String, String> params = new LinkedHashMap<>();
             request.getParameterMap().forEach((key, values) -> params.put(key, values[0]));
@@ -37,26 +36,38 @@ public class UpdateTelefoneServlet extends HttpServlet {
 
             Telefone model = new Telefone(params);
             //VALIDAÇAO DE ERRO;
+            List<TelefoneView> telefoneViews = telefoneDAO.listarComEmpresa();
             if (!Regex.validarTelefone(model.getCNrTelefone())) {
-                ErroServlet.setErro(request, response, dao,
-                        "Não foi possível cadastrar Telefone! Insira um Telefone válido", lista, caminho);
+                telefoneViewSetErro(request, response, telefoneDAO,telefoneViews,
+                        "Não foi possível atualizar Telefone! Insira um Telefone válido", lista, caminho);
                 return;
             }
             if (empresa == null) {
-                ErroServlet.setErro(request, response, dao,
-                        "Não foi possível cadastrar Telefone! Insira uma empresa cadastrada anteriormente", lista, caminho);
+                telefoneViewSetErro(request, response, telefoneDAO, telefoneViews,
+                        "Não foi possível atualizae Telefone! Insira uma empresa cadastrada anteriormente", lista, caminho);
+                return;
+            }
+            if (empresa.getCAtivo() != '1') {
+                telefoneViewSetErro(request, response, telefoneDAO, telefoneViews, "Nao foi possivel atualizar Telefone! Insira uma empresa ativa" , lista, caminho);
                 return;
             }
             model.setNCdTelefone(Integer.parseInt(params.get("nCdTelefone")));
             model.setCNrTelefone(params.get("cNrTelefone"));
             model.setCDescricao(params.get("cDescricao"));
 
-            dao.update(model);
+            telefoneDAO.update(model);
             response.sendRedirect(request.getContextPath() + "/telefone/list");
         } catch (ConnectionFailedException | NotFoundException  e) {
             request.setAttribute("erro", "Erro ao atualizar Telefone: " + e.getMessage());
             RequestDispatcher rd = request.getRequestDispatcher("/erro.jsp");
             rd.forward(request, response);
         }
+    }
+    public void telefoneViewSetErro(HttpServletRequest request, HttpServletResponse response, TelefoneDAO telefoneDAO, List<TelefoneView> telefoneView, String mensagem, String lista, String caminho)
+            throws jakarta.servlet.ServletException, IOException {
+        telefoneView = telefoneDAO.listarComEmpresa();
+        request.setAttribute(lista, telefoneView);
+        request.setAttribute("erro", mensagem);
+        request.getRequestDispatcher(caminho).forward(request, response);
     }
 }
