97f3aa3ac9791d6e691ca16529b6d0c0e5596a75
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2025-11-02 20:15:03 -0300

    (bugfix) Corrige construtor de NotFoundException

--- src/main/java/com/purpura/exception/NotFoundException.java
+++ src/main/java/com/purpura/exception/NotFoundException.java
@@ -1,11 +1,11 @@
 package com.purpura.exception;
 
 public class NotFoundException extends PurpuraException {
-    public NotFoundException(String nomeTabela, Object id, Throwable cause) {
+    public NotFoundException(String nomeTabela, Object id) {
         super(
             String.format("Registro não encontrado: tabela=%s id=%s", nomeTabela, id),
             "Registro não encontrado.",
-            "NOT_FOUND", cause
+            "NOT_FOUND"
         );
     }
 }
